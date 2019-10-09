149eb0dbcfb076a6274c72ea3cd5affcc2306638
Dima-wq <Hodarenokd@gmail.com>
2019-10-09 18:06:37 +0300

    Updates

---
+++ precache-manifest.671443014bec3e34b276b28d4378d2ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76c7e8fef70b65310485c9938d2993b3",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "4495ec42cce0f319235c",
+    "url": "/Portfolio/static/css/main.dab8b578.chunk.css"
+  },
+  {
+    "revision": "9357da6f7380d159cd52",
+    "url": "/Portfolio/static/js/2.341cf635.chunk.js"
+  },
+  {
+    "revision": "4495ec42cce0f319235c",
+    "url": "/Portfolio/static/js/main.8af2dbfc.chunk.js"
+  },
+  {
+    "revision": "efb536f25f40738a93c8",
+    "url": "/Portfolio/static/js/runtime-main.60e60d4b.js"
+  }
+]);
